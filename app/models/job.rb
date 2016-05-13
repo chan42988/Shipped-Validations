93908836706bbd130c04eb6c238bc967488e985a
Ohad Yarel <ohadyarel@gmail.com>
2016-05-13 13:23:19 -0400

    adding validations for Job model

--- app/models/job.rb
+++ app/models/job.rb
@@ -3,4 +3,11 @@ class Job < ActiveRecord::Base
 	belongs_to :user
 	belongs_to :boat
 
+	validates_presence_of :name, on: :create
+	validates_presence_of :name, on: :update
+	validates :name, uniqueness: { case_sensitive: false }
+
+	validates :cost, numericality: { :greater_than_or_equal_to: 1000.00 }
+	validates :crago, length: { minimum: 50 }
+
 end
