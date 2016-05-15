5e49d0b4467bf7ed7636a651bd8a4c43612937a9
Ohad Yarel <ohadyarel@gmail.com>
2016-05-15 13:05:34 -0400

    adding UJS to follow/unfollow button on boat/show.html.erb. adding list of boats followed on home/show.html.erb

--- app/controllers/follows_controller.rb
+++ app/controllers/follows_controller.rb
@@ -1,12 +1,12 @@
 class FollowsController < ApplicationController
 
 	def create
-		@follow = Follow.new(user_id: current_user.id, boat_id: params[:follow][:boat_id])
+		@follow = Follow.new(user_id: current_user.id, boat_id: params[:boat_id])
 
 		respond_to do |format|
 			if @follow.save
-				# format.html { redirect_to :back }
-				# format.json { render :show, status: :created, location: @follow }
+				format.html { redirect_to :back }
+				format.json { render :show, status: :created, location: @follow }
 				format.js
 			else
 				format.html { render :new }
@@ -16,12 +16,11 @@ class FollowsController < ApplicationController
 	end
 
 	def destroy
-		# Follow.where(user_id: current_user.id, boat_id: params[:follow][:boat_id]).delete
 		@follow = Follow.find(params[:id])
 		@follow.destroy
 		respond_to do |format|
-			# format.html { redirect_to :back, notice: 'You are not following this boat anymore.'}
-			# format.json { head :no_content }
+			format.html { redirect_to :back }
+			format.json { head :no_content }
 			format.js
 		end 
 	end
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,10 +1,8 @@
 class HomeController < ApplicationController
 	def show
-
-			@user = User.find(current_user.id)
-			@boat = Boat.new
-			@boats = Boat.where(user_id: current_user.id)
-	
-		
+		@user = User.find(current_user.id)
+		@boat = Boat.new
+		@boats = Boat.where(user_id: current_user.id)
+		@follows = Follow.where(user_id: current_user.id)
 	end
 end
